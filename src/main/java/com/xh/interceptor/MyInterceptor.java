11e5a8f471a5c1862f1fa73d982b945387e2a4b5
chevalierxh <dengxing.hai@163.com>
2019-12-16 00:42:15 +0800

    其他账号修改

--- src/main/java/com/xh/interceptor/MyInterceptor.java
+++ src/main/java/com/xh/interceptor/MyInterceptor.java
@@ -5,15 +5,15 @@ import java.lang.reflect.Method;
 public class MyInterceptor implements Interceptor {
 
     public boolean before(Object proxy, Object target, Method method, Object[] args) {
-        System.out.println("反射方法前调用");
+        System.out.println("反射方法前调用.");
         return false;
     }
 
     public void around(Object proxy, Object target, Method method, Object[] args) {
-        System.out.println("取代了被代理对象的方法");
+        System.out.println("取代了被代理对象的方法.");
     }
 
     public void after(Object proxy, Object target, Method method, Object[] args) {
-        System.out.println("反射方法后调用");
+        System.out.println("反射方法后调用.");
     }
 }
